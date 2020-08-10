ab476ad51b3a13a97d9c71308ba7cb0d59f89c20
Brian Broll <brian.broll@gmail.com>
2020-08-10 11:39:02 -0500

    Reword comment

--- source/rlinterface/RLInterface.h
+++ source/rlinterface/RLInterface.h
@@ -48,9 +48,9 @@ struct mg_context;
 /**
  * Implements an interface providing fundamental capabilities required for reinforcement
  * learning (over HTTP). This primarily consists of enabling an external script to configure
- * the scenario (via Reset) and then step the game engine manually optionally applying
- * player actions (via Step). The interface also supports querying unit templates to provide
- * information about max health and other potentially relevant game state information.
+ * the scenario (via Reset) and then step the game engine manually and apply player actions
+ * (via Step). The interface also supports querying unit templates to provide information
+ * about max health and other potentially relevant game state information.
  */
 class RLInterface
 {
